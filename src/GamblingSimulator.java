acd8389c280721e16ca638ae291e706ebf5e17ae
vismaya01 <kvvismaya98@gmail.com>
2020-12-03 10:23:56 +0530

    Added: win or loss

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,5 +1,18 @@
 
 public class GamblingSimulator {
-	public int STAKE = 100;
-	public int BET_AMOUNT = 1;
+	public final int STAKE = 100;
+	public final int BET_AMOUNT = 1;
+	
+	public void winOrLoss() {
+		if((Math.floor(Math.random()*10) % 2) == BET_AMOUNT) {
+			System.out.println("Win");
+		}
+		else {
+			System.out.println("Loss");
+		}
+	}
+	public static void main(String[] args) {
+		GamblingSimulator gambler = new GamblingSimulator();
+		gambler.winOrLoss();
+	}
 }
