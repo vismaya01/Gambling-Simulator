74294816cf5b5538c3e45df902555c09b52ad474
vismaya01 <kvvismaya98@gmail.com>
2020-12-03 11:59:46 +0530

    Added: total amount win or loss in 20 days

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -3,30 +3,47 @@ public class GamblingSimulator {
 	//UC1
 	public static  final int STAKE = 100;
 	public static final int BET_AMOUNT = 1;	
+	public static int fiftyPercentageLoss = (STAKE / 100) * 50;
+	public static int fiftyPercentageWin =STAKE + (STAKE / 100) * 50;
 	//UC2
-	public static void winOrLoss() {
+	public static boolean winOrLoss() {
 		if((Math.floor(Math.random()*10) % 2) == BET_AMOUNT) {
-			System.out.println("Win");
+			return true;
 		}
 		else {
-			System.out.println("Loss");
+			return false;
 		}
 	}
 	//UC3
-	public static void resignDay() {
+	public static int resignDay() {
 		int cash = STAKE;
-		while(cash < 150 && cash > 50) {
-			if((Math.floor(Math.random()*10) % 2) == BET_AMOUNT) {
+		while(cash < fiftyPercentageWin && cash > fiftyPercentageLoss) {
+			if(winOrLoss()) {
 				cash = cash + 1;
 			}
 			else {
 				cash = cash -1;
 			}
 		}
-		System.out.println(cash);
+		return cash;
+	}
+	//UC4
+	public static void toatalAmount() {
+		int day = 1;
+		int money = 0;
+		while(day <= 20) {
+			if(resignDay() == fiftyPercentageWin ){
+				money = fiftyPercentageWin - STAKE;
+				System.out.println("amount win on day "+day+" is "+money);
+			}
+			else {
+				money = STAKE - fiftyPercentageLoss;
+				System.out.println("amount loss on day "+day+" is "+money);
+			}
+			day = day + 1;
+		}
 	}
 	public static void main(String[] args) {
-		winOrLoss();
-		resignDay();
+		toatalAmount();
 	}
 }
