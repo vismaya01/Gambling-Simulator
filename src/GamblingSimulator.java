d715f9b6a5c2ef75aacd38f083f7b700d89dc311
vismaya01 <kvvismaya98@gmail.com>
2020-12-03 11:01:57 +0530

    Added: 50% win or loss would resigin for the day

--- src/GamblingSimulator.java
+++ src/GamblingSimulator.java
@@ -1,9 +1,10 @@
 
 public class GamblingSimulator {
-	public final int STAKE = 100;
-	public final int BET_AMOUNT = 1;
-	
-	public void winOrLoss() {
+	//UC1
+	public static  final int STAKE = 100;
+	public static final int BET_AMOUNT = 1;	
+	//UC2
+	public static void winOrLoss() {
 		if((Math.floor(Math.random()*10) % 2) == BET_AMOUNT) {
 			System.out.println("Win");
 		}
@@ -11,8 +12,21 @@ public class GamblingSimulator {
 			System.out.println("Loss");
 		}
 	}
+	//UC3
+	public static void resignDay() {
+		int cash = STAKE;
+		while(cash < 150 && cash > 50) {
+			if((Math.floor(Math.random()*10) % 2) == BET_AMOUNT) {
+				cash = cash + 1;
+			}
+			else {
+				cash = cash -1;
+			}
+		}
+		System.out.println(cash);
+	}
 	public static void main(String[] args) {
-		GamblingSimulator gambler = new GamblingSimulator();
-		gambler.winOrLoss();
+		winOrLoss();
+		resignDay();
 	}
 }
